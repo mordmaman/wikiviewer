6f8fa8b05ac08b17f5a5c3fc05d07e0ee0a11773
Mord Maman <jaime.mord.maman@gmail.com>
2017-01-16 20:59:07 +0000

    adds a getJSON request but not quite working

--- styles.js
+++ styles.js
@@ -1,6 +1,23 @@
 $( document ).ready(function() {
     console.log( "ready!" );
     $("#random").click(function(){
-    console.log("The paragraph was clicked.");
+                $.getJSON("https://en.wikipedia.org/wiki/Special:Random&callback=?",function(data){
+                console.log(data);
+                });
+        // $.getJSON("https://en.wikipedia.org/wiki/Special:Random", function(json) {
+        // console.log(html(JSON.stringify(json)));
     });
 });
+
+// $.getJSON("/json/cats.json", function(json) {
+//   $(".message").html(JSON.stringify(json));
+// });
+
+// var xhr = new XMLHttpRequest();
+// xhr.open('GET', 'http://projectwhatup.us:5000/api/posts', false);
+// xhr.send();
+// xhr.responseText;
+
+// $.getJSON("https://api.github.com/users/jeresig?callback=?",function(json){
+//   console.log(json);
+// });
